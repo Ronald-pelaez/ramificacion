0dfafb9be635cfa4616b252da986845e24e8f731
Ronald Jimenez <Ronaldpelaez201@gmail.com>
2024-11-18 10:41:09 -0500

    feat: "VSFT-9223-intento de hacer commit y push directamente en desarrollo"

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
 
 echo "rama feature-VSFT-9223";
-echo "En esta rama tengo el desarrollo, que actualmente no voy a poder ver en la rama main";
-echo "Este echo lo agrego 1 para el último commit";
-echo "Este echo lo agrego 2 para el último commit";
-
-echo "intento de hacer commit y push directamente en main desde desarrollo";
+echo "aqui hago un commit y push pero directamente en mi rama de desarrollo";
