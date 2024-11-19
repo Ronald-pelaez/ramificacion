3eef497e58395dc847fb549b21104a7eae7f1023
Ronald Jimenez <Ronaldpelaez201@gmail.com>
2024-11-19 15:24:01 -0500

    fix: resolver bug

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "rama feature-VSFT-9223";
+echo "rama main";
 echo "En esta rama tengo el desarrollo, que actualmente no voy a poder ver en la rama main";
 echo "Este echo lo agrego para el último commit";
