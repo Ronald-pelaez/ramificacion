648bf5b83dd3a388ca7bd6753e35a17f359cb012
Ronald Jimenez <Ronaldpelaez201@gmail.com>
2024-11-28 16:57:45 -0500

    docs: "VSFT-2233-trabajo dia 2 con docs"

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ echo "proyecto principal, rama main";
 echo "aquí soluciono un bug que había en main desde la rama 1012";
 
 echo "trabajo dia 1-con doc";
-echo "trabajo dia 2-2233";
+echo "trabajo dia 2-con doc";
 echo "trabajo listo-2233";
