2d760598bf9a5d521ab9faacb7e3406afcf029c4
Ronald Jimenez <Ronaldpelaez201@gmail.com>
2024-11-28 16:49:19 -0500

    docs: "VSFT-1122-trabajo dia 2 con docs"

--- function.php
+++ function.php
@@ -4,5 +4,5 @@ echo "Esta es una nueva función que voy a a gregar a main";
 echo "modificacion botón de pago";
 
 echo "pasarela commit 1 con doc";
-echo "pasarela commit 2 prueba";
+echo "pasarela commit 2 con doc";
 echo "solucion error pasarela prueba con doc 1";
