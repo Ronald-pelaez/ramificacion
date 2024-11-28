2f447f0764025752b3871d59a61d1cd8006a616e
Ronald Jimenez <Ronaldpelaez201@gmail.com>
2024-11-28 16:36:38 -0500

    fix: "VSFT-1122-solucion pasarela de pago prueba con doc 1"

--- function.php
+++ function.php
@@ -4,5 +4,5 @@ echo "Esta es una nueva función que voy a a gregar a main";
 echo "modificacion botón de pago";
 
 echo "pasarela commit 1";
-echo "pasarela commit 2";
+echo "pasarela commit 2 prueba";
 echo "solucion error pasarela prueba con doc 1";
