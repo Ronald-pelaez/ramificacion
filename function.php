0a5a3a85c84cfb8b7001894723c9395efc9f2ba2
Ronald Jimenez <Ronaldpelaez201@gmail.com>
2024-11-28 16:02:54 -0500

    fix: "VSFT-1122-pasarela commit 2"

--- function.php
+++ function.php
@@ -4,5 +4,5 @@ echo "Esta es una nueva función que voy a a gregar a main";
 echo "modificacion botón de pago";
 
 echo "pasarela commit 1";
-echo "trabajo dia 2 function";
+echo "pasarela commit 2";
 echo "trabajo listo, solucion de bug en function";
