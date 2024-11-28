e8a88e2b4716ce9f06ca2125320d469420df8c45
Ronald Jimenez <Ronaldpelaez201@gmail.com>
2024-11-28 12:38:47 -0500

    fix: "VSFT-9210-trabajo dia 2"

--- index.php
+++ index.php
@@ -6,4 +6,4 @@ echo "aquí soluciono un bug que había en main desde la rama 1012";
 
 echo "Aquí estoy haciendo el trabajo del primer commit";
 
-
+echo "Aquí estoy haciendo el trabajo del segundo commit";
