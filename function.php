2c2b267d77cbdefc59dc110b4ccae21628ab01b5
Ronald Jimenez <Ronaldpelaez201@gmail.com>
2024-11-28 18:16:38 -0500

    fix: "VSFT-2233- solucion imagen no carga"

--- function.php
+++ function.php
@@ -4,5 +4,5 @@ echo "Esta es una nueva función que voy a a gregar a main";
 echo "modificacion botón de pago";
 
 echo "trabajo dia 1 - function";
-echo "trabajo dia 2 function";
-echo "trabajo listo, solucion de bug en function";
+echo "trabajo dia 2 - function";
+echo "solucion de bug en function";
