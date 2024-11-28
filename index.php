9724c4fd78e73f5482545d7f60cdaf6f14b6ea16
Ronald Jimenez <Ronaldpelaez201@gmail.com>
2024-11-28 16:09:09 -0500

    fix: "VSFT-2233-trabajo 1-2223"

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
 echo "proyecto principal, rama main";
 echo "aquí soluciono un bug que había en main desde la rama 1012";
 
-echo "trabajo dia 1";
+echo "trabajo dia 1-2223";
 echo "trabajo dia 2";
 echo "trabajo listo";
