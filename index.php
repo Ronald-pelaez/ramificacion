949bc53d2c3710d7523a5f23e3a36d5088918a03
Ronald Jimenez <Ronaldpelaez201@gmail.com>
2024-11-28 12:39:50 -0500

    fix: "VSFT-9210-solucion bug en formulario"

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ echo "proyecto principal, rama main";
 echo "aquí soluciono un bug que había en main desde la rama 1012";
 
 echo "Aquí estoy haciendo el trabajo del primer commit";
-
 echo "Aquí estoy haciendo el trabajo del segundo commit";
+echo "aquí ya soluciono el bug";
