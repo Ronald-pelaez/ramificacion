1667d8c7a7eac01f0f32fdbdedf51e9b0e32d8af
Ronald Jimenez <Ronaldpelaez201@gmail.com>
2024-11-28 16:58:17 -0500

    fix: "VSFT-2233-solucion error IMG"

--- index.php
+++ index.php
@@ -6,4 +6,4 @@ echo "aquí soluciono un bug que había en main desde la rama 1012";
 
 echo "trabajo dia 1-con doc";
 echo "trabajo dia 2-con doc";
-echo "trabajo listo-2233";
+echo "trabajo listo-con fix";
