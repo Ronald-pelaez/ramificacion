710da67048666a7abd75f04744c40388848671d8
Ronald Jimenez <Ronaldpelaez201@gmail.com>
2024-11-28 15:25:36 -0500

    fix: "VSFT-9210-trabajo dia 1"

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
 echo "proyecto principal, rama main";
 echo "aquí soluciono un bug que había en main desde la rama 1012";
 
-echo "Aquí estoy haciendo el trabajo del primer commit";
+echo "trabajo dia 1";
 echo "Aquí estoy haciendo el trabajo del segundo commit";
 echo "aquí ya soluciono el bug";
