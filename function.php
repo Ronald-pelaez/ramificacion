e6a79bead742d88efc6a6d1f78f0db924065ea16
Ronald Jimenez <Ronaldpelaez201@gmail.com>
2024-11-28 18:14:45 -0500

    docs: "VSFT-2233- trabajo de dia 1 con docs"

--- function.php
+++ function.php
@@ -3,6 +3,6 @@
 echo "Esta es una nueva función que voy a a gregar a main";
 echo "modificacion botón de pago";
 
-echo "trabajo dia 1 function";
+echo "trabajo dia 1 - function";
 echo "trabajo dia 2 function";
 echo "trabajo listo, solucion de bug en function";
