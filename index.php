5fea8efea826dc9e1066a4b6ac73cc269b4d5981
Ronald Jimenez <Ronaldpelaez201@gmail.com>
2024-12-06 15:50:11 -0500

    feat: "(VSFT-123)-breveDescripciónEnCamellCase"

--- index.php
+++ index.php
@@ -7,6 +7,6 @@ echo "aquí soluciono un bug que había en main desde la rama 1012";
 echo "trabajo dia 1";
 echo "trabajo dia 2";
 echo "trabajo listo";
-echo " con release gitbook";
+echo "Prueba con release gitbook";
 
 
